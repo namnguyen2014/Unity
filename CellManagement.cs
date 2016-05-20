31a031ecd391502f8b267a9e86f3fada87582819
Nam Nguyen <hoainamvn2014@gmail.com>
2016-05-20 16:29:50 +0700

    Truc Xanh
    Init Cell, Click

---
+++ CellManagement.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CellManagement : MonoBehaviour {
+
+	public GameObject cellPrefab;
+	public Transform rootPanel;
+
+	public Sprite[] imgArray;
+	private List<PrefabOnClick> lstController = new List<PrefabOnClick>();
+
+	void Start () {
+		for(int i = 0; i < 16; i++)
+		{
+			GameObject create = Instantiate(cellPrefab);
+			create.transform.SetParent(rootPanel);
+			create.GetComponent<PrefabOnClick>().cell = this;
+			lstController.Add(create.GetComponent<PrefabOnClick>());
+		}
+		InitSprite();
+	}
+
+	void InitSprite()
+	{
+		for(int i = 0; i < lstController.Count; i++)
+		{
+			int index = Random.Range(0, imgArray.Length);
+			while(CheckImgIndex(imgArray[index]) >= (lstController.Count/imgArray.Length))
+			{
+				index = Random.Range(0,imgArray.Length);
+			}
+			lstController[i].img_2 = imgArray[index]; 
+
+		}
+	}
+
+	int CheckImgIndex (Sprite sprites)
+	{
+		int currCount = 0;
+		for(int i = 0; i < lstController.Count; i++)
+		{
+			if(sprites == lstController[i].img_2)
+			{
+				currCount++;
+			}
+			if(currCount >= (lstController.Count/imgArray.Length))
+			{
+				return currCount;
+			}
+		}
+		return currCount;
+	}
+
+	public void CheckSameImg(PrefabOnClick check)
+	{
+		for(int i = 0; i < lstController.Count; i++)
+		{
+			if(lstController[i].imgCell.sprite == check.imgCell.sprite)
+			{
+				//lstController[i].GetComponent<GameObject>().SetActive= false;
+				lstController[i].GetComponent<SpriteRenderer>().enabled = false;
+				//check.GetComponent<GameObject>().SetActive= false;
+				check.GetComponent<SpriteRenderer>().enabled = false;
+			}
+		}
+	}
+}
---
+++ PrefabOnClick.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PrefabOnClick : MonoBehaviour {
+	public Sprite img_1;
+	public Sprite img_2;
+	public Image imgCell;
+	public CellManagement cell;
+
+	private float delayTime = 5;
+
+	public void Start()
+	{
+		imgCell = GetComponent<Image>(); 
+	}
+
+	public void Update()
+	{
+		if(delayTime > 0)
+		{
+			delayTime -= Time.deltaTime;
+		}
+
+	}
+
+	public void OnMouseDown()
+	{
+		ChangeSprite();
+	}
+
+	public void ChangeSprite()
+	{
+			if(imgCell.sprite == img_1)
+			{
+				imgCell.sprite = img_2;
+
+			}
+			else 
+			{
+				imgCell.sprite = img_1;
+			}
+	}
+}
